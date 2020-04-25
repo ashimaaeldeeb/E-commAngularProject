f90f57f2cede38c0e2a54a6f6bb3802c50a89f63
rawanshisht <rawan.shisht@gmail.com>
2020-04-26 00:47:31 +0200

    cart-rawan

--- routes/carts.js
+++ routes/carts.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const Cart = require("../models/cart");
 const User = require("../models/user");
+const Product = require("../models/product");
 
 // const validateUser = require("../helpers/validateUser");
 const validateCart = require("../helpers/validateCart");
@@ -15,7 +16,14 @@ router.get("/user/:id", async (req, res) => {
   const { error } = validateObjectId(req.params.id);
   if (error) return res.status(400).send("User id is not valid");
   //   const cart = await Cart.find(p => p.id === parseInt(req.params.id) && p => p.productsList.isDeleted !== true);
-  const cart = await Cart.find({ _id: id, isDeleted: true });
+  const cart = await Cart.find({ userId: id });
+  console.log("prod id", cart);
+
+  const productsId = [];
+  for(let i=0; i<cart[0].productsList.length; i++){
+    productsId.push(cart[0].productsList[i].productId)
+  }
+  console.log("prod id", productsId);
   if (!cart) return res.status(404).send("Cart is not found for this user.");
 
   res.status(200).send(cart);
@@ -23,70 +31,75 @@ router.get("/user/:id", async (req, res) => {
 
 //Post product to user's cart
 router.post("/user/:id", validateCart, async (req, res) => {
-
-  const { error } = validateObjectId(req.params.id);
-  if (error) return res.status(400).send("User id is not valid");
-
-  const user = await User.findById(id);
-  if (!user) return res.status(400).send("User is not found");
-
-  let productInCart = new Cart({
-    ...req.body
-  });
-
-  const cart = await Cart.find({ userId: productInCart.userId});
-  if (!cart) return res.status(400).send("User's cart is not found");
-
-  cart.productsList.push(productInCart); //keda badef elid beta3 elcart di 3nd elref cart 3nd eluser
-
-  await cart.save();
-
-  res.status(200).send(productInCart);
-});
-
-//Patch user's cart
-router.patch("/user/:id/product", validateCart, async (req, res) => {
-
-  const {id } = req.params;
-  const { error } = validateObjectId(req.params.id);
-  if (error) return res.status(400).send("User id is not valid");
-
-  const user = await User.findById(id);
-  if (!user) return res.status(400).send("User is not found");
-
-  const cartOfUser = new Cart({
-    ...req.body
-  });
-
-  const cart = await Cart.find({ userId: id});
-  if (!cart) return res.status(400).send("User's cart is not found");
-
-
-  cart = { ...cart, cartOfUser };
-
-  await cartOfUser.save();
-
-  res.status(200).send(user);
-});
-
-//Delete a product is user's cart
-router.delete("/user/:id/product", async (req, res) => {
-
   const { error } = validateObjectId(req.params.id);
   if (error) return res.status(400).send("User id is not valid");
 
   const user = await User.findById(req.params.id);
-    // const cart = await Cart.findById(req.params.id); ?????????
   if (!user) return res.status(400).send("User is not found");
 
-  const cart = await Cart.find({ userId: id});
+  const userCart = await Cart.findById(user.cart); //elcart beta3t eluser dah
+  if (!userCart) return res.status(400).send("User's cart is not found");
+
+  userCart.productsList.push(...req.body.productsList);
+
+  await userCart.save();
+
+  res.status(200).send(userCart);
+});
+
+//Patch user's cart
+router.put("/user/:id/product", validateCart, async (req, res) => {
+  const { id } = req.params;
+  const { error } = validateObjectId(req.params.id);
+  if (error) return res.status(400).send("User id is not valid");
+
+  const user = await User.findById(id);
+  if (!user) return res.status(400).send("User is not found");
+
+  let cart = await Cart.find({ userId: id });
   if (!cart) return res.status(400).send("User's cart is not found");
 
-  cart.productsList.isDeleted = true;
+  const updateCart = await Cart.findByIdAndUpdate(user.cart, req.body, {
+    new: true
+  });
 
-  await cart.save();
+  await updateCart.save();
 
-  res.status(200).send("Product is deleted successfully");
+  res.status(200).send(updateCart);
+});
+
+//Delete a product is user's cart
+router.delete("/user/:id/product/:productId", async (req, res) => {
+  const { error } = validateObjectId(req.params.id);
+  if (error) return res.status(400).send("User id is not valid");
+
+  const { error2 } = validateObjectId(req.params.productId);
+  if (error2) return res.status(400).send("Product id is not valid");
+
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(400).send("User is not found");
+
+  let cart = await Cart.find({ userId: req.params.id });
+  if (!cart) return res.status(400).send("User's cart is not found");
+
+  const product = await Product.find({ id: req.params.productId });
+  if (!product) return res.status(400).send("Product ID is not found");
+
+  const newCartItems = cart[0].productsList.filter(c=> c.productId != req.params.productId);
+  cart[0].productsList = [];
+
+  for(let i=0; i<newCartItems.length; i++){
+    cart[0].productsList.push(newCartItems[i]);
+  }
+  console.log("temp", cart[0].productsList);
+
+  const updateCart = await Cart.findByIdAndUpdate(user.cart, [...cart], {
+    new: true
+  });
+
+  await updateCart.save();
+  res.status(200).send(cart);
+
 });
 
 module.exports = router;
