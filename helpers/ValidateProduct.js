6036ed9b413d6961cb153761e2b04544eb56ad0b
basmagamil <basma.gamil17@gmail.com>
2020-04-28 14:43:22 +0200

    validate-product-edit

--- helpers/ValidateProduct.js
+++ helpers/ValidateProduct.js
@@ -3,17 +3,17 @@ Joi.objectId = require('joi-objectid')(Joi);
 
 const productSchema = Joi.object({
     title: Joi.string().required(),
-    images: Joi.array().items(Joi.string().required()).default([]),
+    images: Joi.array().items(Joi.string().allow('')).default([]),
     price: Joi.number().required(),
     details:Joi.object({
-        Brand: Joi.string(),
-        Processor: Joi.string(),
-        RAM: Joi.string(),
-        HardDisk: Joi.string(),
-        GPU: Joi.string(),
-        Color: Joi.string()
+        Brand: Joi.string().optional().allow(''),
+        Processor: Joi.string().optional().allow(''),
+        RAM: Joi.string().optional().allow(''),
+        HardDisk: Joi.string().optional().allow(''),
+        GPU: Joi.string().optional().allow(''),
+        Color: Joi.string().optional().allow('')
     }),
-    ratioOfPromotion: Joi.number(),
+    ratioOfPromotion: Joi.number().optional(),
     isPromoted: Joi.boolean().default(false),
     quantity: Joi.number().required(),
     isDeleted: Joi.boolean().default(false),
